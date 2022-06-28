b5ce90a679c94e0b8cbbe8275545820a9aa05633
Flo0406 <89452934+Flo0406@users.noreply.github.com>
2022-06-28 18:55:47 +0200

    Update Bildempfang.py

--- Bildempfang.py
+++ Bildempfang.py
@@ -1,4 +1,3 @@
-
 import socket
 import base64
 import io
@@ -6,27 +5,21 @@ import os
 import time
 import glob
 import requests
-import telepot  # telegram bot
+import telepot #telegram bot
 from telepot.loop import MessageLoop
-#import PIL.Image as Image
 from datetime import datetime
 from io import BytesIO
 from PIL import Image, ImageFile
 from shutil import copyfile
-from _thread import *
 
 
-host = ''  # this is the server
-port = ""  # port that ist used for this connection
+
+host = '' #this is the server
+port = your port # port that is used for this connection
 size = 0
 
-token = ""
-
-chatid_basti = ""  # chatid of my telegram
-chatid = ""
-chatid_yaacoub = ""
-chatid_nils = ""
-
+token = "token of your selfmade bot" #token from bot
+chatid = your chatid #chatid of my telegram
 
 # Telepot Settings
 bot = telepot.Bot(token)
@@ -34,147 +27,135 @@ bot.getMe()
 bot.getUpdates()
 
 
-def setupServer():  # this function creates a socket using the internet
+def setupServer(): #this function creates a socket using the internet
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("Socket created.")
     try:
-        # makes sure we dont get "address already used" error
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)#makes sure we dont get "address already used" error
         s.bind((host, port))
     except socket.error as msg:
         print(msg)
     print("Socket bind complete.")
     return s
 
-
-def setupConnection():  # this function set up the connection to the client
-    s.listen(3)  # Allows one connection at a time.
-    conn, address = s.accept()  # accept the client
+def setupConnection(): #this function set up the connection to the client
+    s.listen(3) # Allows one connection at a time.
+    conn, address = s.accept() #accept the client
     print("Connected to: " + address[0] + ":" + str(address[1]))
     return conn
 
-
-def GETSIZE(conn):  # this function return the size of the data that we will receive
-    data = conn.recv(1024)  # receive the data
-    size = data.decode('utf-8')  # decode
-    # send 'ok' because we are ready to receive the big data
-    conn.sendall(str.encode('ok'))
+def GETSIZE(conn): # this function return the size of the data that we will receive
+    data = conn.recv(1024) # receive the data
+    size = data.decode('utf-8') #decode
+    conn.sendall(str.encode('ok')) #send 'ok' because we are ready to receive the big data
     print(size)
     size = int(size)
     return size
-
-
-def imageTransfer(conn, size, cam):  # this function is used to receive and save images
+    
+def imageTransfer(conn, size, cam): #this function is used to receive and save images
     now = datetime.now()
     currenttime = now.strftime("%Y%m%d%H%M%S")
-    # name is build from Year,Month,Day,Hour,Minute,Second
-    picturename = currenttime + "Picture.jpg"
+    picturename = currenttime + "Picture.jpg" # name is build from Year,Month,Day,Hour,Minute,Second
     print(picturename)
-    # need to prevent from "truncated" error
-    ImageFile.LOAD_TRUNCATED_IMAGES = True
-    imagedata = conn.recv(2048)  # receive imagedata
+    ImageFile.LOAD_TRUNCATED_IMAGES = True # need to prevent from "truncated" error
+    imagedata = conn.recv(2048) #receive imagedata
     data = imagedata
-    if imagedata:
-        while imagedata:  # while we receive data do this
+    if imagedata: 
+        while imagedata: # while we receive data do this
             imagedata = conn.recv(2048)
-            # print(len(imagedata))
-            data += imagedata  # add received data to data store
+            #print(len(imagedata))
+            data += imagedata # add received data to data store
             if len(data) == size:
                 break
-    image = Image.open(io.BytesIO(data))  # put data into image
-    image.save(picturename)  # save Image with given picturename
+    image = Image.open(io.BytesIO(data)) # put data into image
+    image.save(picturename) #save Image with given picturename
     image.save('CAM'+str(cam)+'.jpg')
-    os.system('mv /home/pi/'+picturename+' /home/pi/CAM'+str(cam) +
-              '/Images/')  # move picture to provided directory
-    os.system('sudo mv /home/pi/CAM'+str(cam)+'.jpg /home/pi/CAM' +
-              str(cam)+'/Images/')  # move picture to provided directory
+    os.system('mv /home/pi/'+picturename+' /home/pi/CAM'+str(cam)+'/Images/')#move picture to provided directory
+    os.system('sudo mv /home/pi/CAM'+str(cam)+'.jpg /home/pi/CAM'+str(cam)+'/Images/')#move picture to provided directory
     print("Image saved")
-    folder = '/home/pi/CAM'+str(cam)+'/'
+    folder = '/home/pi/CAM'+str(cam)+'/Images/'
     dirlist = os.listdir(folder)
     numberofimages = len(dirlist)
     if numberofimages == 10:
         print(numberofimages)
-        dirName = '/home/pi/CAM'+str(cam)+'/Images'  # + camera + '/photos'
-        listOfFiles = sorted(filter(lambda x: os.path.isfile(
-            os.path.join(dirName, x)), os.listdir(dirName)))
+        dirName = '/home/pi/CAM'+str(cam)+'/Images' # + camera + '/photos'
+        listOfFiles = sorted(filter (lambda x: os.path.isfile(os.path.join(dirName, x)), os.listdir(dirName)))
         os.remove('/home/pi/CAM'+str(cam)+'/Images/'+listOfFiles[0])
 
 
-def videoTransfer(conn, size, cam):  # this function is used to receive and save videos
+def videoTransfer(conn, size, cam): #this function is used to receive and save videos
+    global noimage    
     now = datetime.now()
     currenttime = now.strftime("%Y%m%d%H%M%S")
-    # name is build from Year,Month,Day,Hour,Minute,Second
-    videoname = currenttime + "Video.mp4"
+    videoname = currenttime + "Video.mp4" # name is build from Year,Month,Day,Hour,Minute,Second
     print(videoname)
     videodata = conn.recv(4096)
-    data = videodata
+    data = videodata   
     if videodata:
-        while len(data) < size:  # as long as data is not the expected size do this
+        while len(data)<size: # as long as data is not the expected size do this
             videodata = conn.recv(4096)
-            print("bytes benoetigt: %d" % int(size)+"\t empfangen bytes: %d" %
-                  len(data), end="\r")  # print how much we received
-            data += videodata  # add received data to datastore
+            print("bytes benoetigt: %d" %int(size)+"\t empfangen bytes: %d" %len(data), end="\r") # print how much we received
+            data += videodata #add received data to datastore
             if len(data) == size:
                 print("done")
                 break
-    video = open('video.h264', 'wb')  # save video in h264 format
+    video = open('video.h264','wb')#save video in h264 format 
     video.write(data)
-    video.close()
-    # convert h264 in mp4 and save with correct name
-    os.system('MP4Box -fps 30 -add video.h264 '+videoname)
+    video.close() 
+    os.system('MP4Box -fps 30 -add video.h264 '+videoname) # convert h264 in mp4 and save with correct name
     print("Video saved")
-    os.system('mv /home/pi/'+videoname+' /home/pi/CAM'+str(cam) +
-              '/Videos/')  # move picture to provided directory
-
+    os.system('mv /home/pi/'+videoname+' /home/pi/CAM'+str(cam)+'/Videos/')#move picture to provided directory
+    noimage = False
 
 def getTemp(cam):
+    global connection
+    conn = connection[cam-1]
     conn.sendall(str.encode('Cam#'+str(cam)+'#temp'))
     measure = conn.recv(4096)
     measure = measure.decode('utf-8')
     measure = measure.split('#', 2)
     temperature = measure[0]
     humidity = measure[1]
-    temp = open('/home/pi/CAM'+str(cam) +
-                '/Measurements/tempcam'+str(cam)+'.txt', 'w')
+    temp = open('/home/pi/CAM'+str(cam)+'/Measurements/tempcam'+str(cam)+'.txt','w')
     temp.write(temperature)
-    hum = open('/home/pi/CAM'+str(cam) +
-               '/Measurements/humcam'+str(cam)+'.txt', 'w')
+    hum = open('/home/pi/CAM'+str(cam)+'/Measurements/humcam'+str(cam)+'.txt','w')
     hum.write(humidity)
 
     print(temperature)
 
-
-def getVideo(cam, length):  # this function is called to ask clients for a "length"long video
-    # send message to all clients
-    conn.sendall(str.encode('Cam#'+str(cam)+'#'+str(length)))
-    size = GETSIZE(conn)  # first call GETSIZE
-    if size > 0:  # when we received a size call videoTransfer
+def getVideo(cam, length): #this function is called to ask clients for a "length"long video
+    global connection
+    global noimage
+    noimage = True
+    conn = connection[cam-1]
+    conn.sendall(str.encode('Cam#'+str(cam)+'#'+str(length))) # send message to all clients 
+    size = GETSIZE(conn) # first call GETSIZE
+    if size > 0: # when we received a size call videoTransfer
         videoTransfer(conn, size, cam)
         size = 0
 
-
-def getImage(cam):  # this function is called to ask clients for a Image
-    # send message to all clients
-    conn.sendall(str.encode('Cam#'+str(cam)+'#pic'))
-    size = GETSIZE(conn)  # first call GETSIZE
-    if size > 0:  # when we received a size call imageTransfer
-        imageTransfer(conn, size, cam)
+def getImage(cam): #this function is called to ask clients for a Image
+    global connection
+    conn = connection[cam-1]
+    conn.sendall(str.encode('Cam#'+str(cam)+'#pic')) # send message to all clients
+    size = GETSIZE(conn) # first call GETSIZE
+    if size > 0: # when we received a size call imageTransfer
+        imageTransfer(conn, size, cam) 
         size = 0
-
 
 def handle(msg):
     global fileBuffer
     global newPhoto
     global cam
 
-    message = msg['text']
-    msg_chatid = msg['chat']['id']
-
+    message=msg['text']
+    msg_chatid=msg['chat']['id']
+    
     if fileBuffer and message.lower() != "ja":
         fileBuffer = False
         bot.sendMessage(chatid, "Das Bild wurde nicht gespeichert.")
 
-    if message.lower() == "/start":
+    if message.lower() == "/start" :
         bot.sendMessage(chatid, 'Die Sicherheitsueberwachung wurde gestartet. \nWenn ein aktuelles Bild einer Kamera geschickt werden soll, antworten Sie mit \"/Kamera*zahl*\". \nSoll eine Aufnahme gestartet werden, antworten Sie mit \"/aufnahme*kamera*,*sekunden* (max. 20sek)\".')
 
     if message[0:9].lower() == "/aufnahme":
@@ -184,7 +165,7 @@ def handle(msg):
             startVideo(cam, message)
         elif message[9] == '2':
             cam = 2
-            #startVideo(cam, message)
+            startVideo(cam, message)
         elif message[9] == '3':
             cam = 3
             #startVideo(cam, message)
@@ -197,152 +178,134 @@ def handle(msg):
     if message[0:7].lower() == "/kamera":
         if len(message) == 8 and int(message[7]) > 0 and int(message[7]) < 5:
             if int(message[7]) == 1:
-                bot.sendMessage(chatid, "1")
                 cam = 1
                 getImage(cam)
 
                 time.sleep(1)
 
                 newPhoto = getNewFile()
-                sendNewestPhoto(newPhoto, cam)
+                sendNewestPhoto(newPhoto, cam) 
                 fileBuffer = True
 
             elif int(message[7]) == 2:
-                bot.sendMessage(chatid, "2")
                 cam = 2
                 getImage(cam)
 
                 time.sleep(1)
 
                 newPhoto = getNewFile()
-                sendNewestPhoto(newPhoto, cam)
+                sendNewestPhoto(newPhoto, cam) 
                 fileBuffer = True
 
             elif int(message[7]) == 3:
-                bot.sendMessage(chatid, "3")
                 cam = 3
                 getImage(cam)
 
                 time.sleep(1)
 
                 newPhoto = getNewFile()
-                sendNewestPhoto(newPhoto, cam)  # Kamera Nummer noch angeben!
+                sendNewestPhoto(newPhoto, cam) # Kamera Nummer noch angeben!
                 fileBuffer = True
 
             elif int(message[7]) == 4:
-                bot.sendMessage(chatid, "4")
                 cam = 4
                 getImage(cam)
 
                 time.sleep(1)
 
                 newPhoto = getNewFile()
-                sendNewestPhoto(newPhoto, cam)  # Kamera Nummer noch angeben!
+                sendNewestPhoto(newPhoto, cam) # Kamera Nummer noch angeben!
                 fileBuffer = True
 
         else:
             bot.sendMessage(chatid, "Die angegebene Kamera existiert nicht.")
 
         if fileBuffer:
-            bot.sendMessage(
-                chatid, "Soll das aktuelle Bild gespeichert werden?")
+            bot.sendMessage(chatid, "Soll das aktuelle Bild gespeichert werden?")
 
     if fileBuffer and message.lower() == "ja":
-        copyfile('/home/pi/CAM' + str(cam) + '/Images/' + newPhoto,
-                 '/home/pi/CAM' + str(cam) + '/Images/TelegramImages/' + newPhoto)
+        copyfile('/home/pi/CAM' + str(cam) + '/Images/' + newPhoto, '/home/pi/CAM' + str(cam) + '/Images/TelegramImages/' + newPhoto)
         fileBuffer = False
-
+        
         bot.sendMessage(chatid, "Das Bild wurde gespeichert.")
+    
 
-
-def getNewFile():  # Camera + Video / Photo?
-    dirName = '/home/pi/CAM' + str(cam) + '/Images'
-    listOfFiles = sorted(filter(lambda x: os.path.isfile(
-        os.path.join(dirName, x)), os.listdir(dirName)))
+def getNewFile(): # Camera + Video / Photo?
+    dirName = '/home/pi/CAM' + str(cam) + '/Images' 
+    listOfFiles = sorted(filter (lambda x: os.path.isfile(os.path.join(dirName, x)), os.listdir(dirName)))
     listOfFiles.reverse()
     return listOfFiles[1]
-
-
-def sendNewestPhoto(fileName, cam):  # Camera und Video / Photo ??
-    document = open('/home/pi/CAM' + str(cam) + '/Images/' + fileName, 'rb')
+        
+def sendNewestPhoto(fileName, cam): # Camera und Video / Photo ??
+    document = open('/home/pi/CAM' + str(cam) + '/Images/' + fileName, 'rb') 
     bot.sendPhoto(chatid, document)
-
 
 def getNewFileVideo():
     dirName = '/home/pi/CAM' + str(cam) + '/Videos'
-    listOfFiles = sorted(filter(lambda x: os.path.isfile(
-        os.path.join(dirName, x)), os.listdir(dirName)))
+    listOfFiles = sorted(filter (lambda x: os.path.isfile(os.path.join(dirName, x)), os.listdir(dirName)))
     listOfFiles.reverse()
     return listOfFiles[0]
-
 
 def sendNewestVideo(fileName, cam):
     document = open('/home/pi/CAM' + str(cam) + '/Videos/' + fileName, 'rb')
     bot.sendVideo(chatid, document)
 
-
 def startVideo(cam, message):
     # if one digit number behind command is added
-    if len(message) == 12 and message[-1].isdigit():
-        getVideo(cam, int(message[-1]))
-        bot.sendMessage(chatid, 'Die Aufnahme wurde fuer ' +
-                        message[-1] + ' Sekunden gestartet. Bitte warten...')
-        time.sleep(int(message[-1])+2)
-        newVideo = getNewFileVideo()
-        sendNewestVideo(newVideo, cam)
-    # if two digit number behind command is added
-    elif len(message) == 13 and message[-2:].isdigit() and int(message[-2:]) <= 20:
-        getVideo(cam, int(message[-2:]))
-        bot.sendMessage(chatid, 'Die Aufnahme wurde fuer ' +
-                        message[-2:] + ' Sekunden gestartet. Bitte warten...')
-        time.sleep(int(message[-2:])+2)
-        newVideo = getNewFileVideo()
-        sendNewestVideo(newVideo, cam)
-    elif len(message) == 13 and message[-2:].isdigit() and int(message[-2:]) > 20:
-        bot.sendMessage(
-            chatid, 'Die maximale Aufnahmezeit betraegt 20 Sekunden.')
-    else:
-        bot.sendMessage(chatid, 'Es wurde keine gueltige Zahl eingegeben.')
-
+        if len(message) == 12 and message[-1].isdigit():
+            bot.sendMessage(chatid, 'Die Aufnahme wurde fuer ' + message[-1] + ' Sekunden gestartet. Bitte warten...')
+            getVideo(cam, int(message[-1]))
+            #time.sleep(int(message[-1])+2)
+            time.sleep(1)
+            newVideo = getNewFileVideo()
+            sendNewestVideo(newVideo, cam)
+        # if two digit number behind command is added
+        elif len(message) == 13 and message[-2:].isdigit() and int(message[-2:]) <= 20: 
+            bot.sendMessage(chatid, 'Die Aufnahme wurde fuer ' + message[-2:] + ' Sekunden gestartet. Bitte warten...')
+            getVideo(cam, int(message[-2:]))
+            #time.sleep(int(message[-2:])+2)
+            time.sleep(1)
+            newVideo = getNewFileVideo()
+            sendNewestVideo(newVideo, cam)
+        elif len(message) == 13 and message[-2:].isdigit() and int(message[-2:]) > 20: 
+            bot.sendMessage(chatid, 'Die maximale Aufnahmezeit betraegt 20 Sekunden.')
+        else:
+            bot.sendMessage(chatid, 'Es wurde keine gueltige Zahl eingegeben.')
 
 s = setupServer()
 connected = False
 video = False
+global noimage
+noimage = False
 
 # Message loop to receive all messages
 MessageLoop(bot, handle).run_as_thread()
 fileBuffer = False
 newPhoto = ''
 cam = 0
-bot.sendMessage(
-    chatid, 'Willkommen beim Security Bot. \n Um die Sicherheitsueberwachung zu starten, geben Sie bitte \"/start\" ein.')
+bot.sendMessage(chatid, 'Willkommen beim Security Bot. \n Um die Sicherheitsueberwachung zu starten, geben Sie bitte \"/start\" ein.')
 
-
-thread_counter = 0
-conn_list = []
-
-def test(connection):
-    print(thread_counter, conn_list)
-    conn_list.append(connection)
-    print(conn_list)
-
-
+alreadyconnected = False
+connection = []
+lastTime = 0
+numberofcams = 0
 while True:
-
-    try:
-        conn = setupConnection()
-
-        start_new_thread(test,(conn,))
-        thread_counter += 1
-        print("Thread Number: " + str(thread_counter))
-        
-        if connected == False:
+    if numberofcams<2:
+        try:
             conn = setupConnection()
-            connected = True
-    except:
-        break
-    if connected:
-        time.sleep(60)
-        # getVideo(1,20)
-        # getImage(1)
-        # getTemp(1)
+            if not conn in connection:
+                connection.append(conn)
+                print("new connection")
+                numberofcams += 1
+        
+        except:
+            for connections in connection:
+                connections.close()
+    else:
+        period = datetime.now()
+        if period.second % 20 == 0 and (abs(period.second - lastTime)) >= 1 and not noimage:
+            print(period)
+            lastTime = period.second
+            for i in range(len(connection)):
+                getImage(i+1)
+                getTemp(i+1)
