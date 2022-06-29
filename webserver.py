3be28543db69fb2102c94f1db84c2ff3f53bfc7b
Flo0406 <89452934+Flo0406@users.noreply.github.com>
2022-06-29 08:48:29 +0200

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,5 +1,5 @@
 from os import system
 
-for i in range(4): # move newest picture to webserver
+for i in range(4): # move newest picture to webserver and change group and user for access
     system("cp /home/pi/CAM"+str(i+1)+"/Images/CAM"+str(i+1)+".jpg /var/www/html/")
     system("sudo chown www-data:www-data /var/www/html/CAM"+str(i+1)+".jpg")
