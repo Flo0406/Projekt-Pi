aa487c6ee52582a7de07c152300dc96e65dce0ef
Flo0406 <89452934+Flo0406@users.noreply.github.com>
2022-06-29 09:43:24 +0200

    Update get_started_client.py

--- get_started_client.py
+++ get_started_client.py
@@ -1,7 +1,15 @@
+#----------------------------------------------------------------------------------------------#
+#Version    : 1.0
+#Date       : 29.06.2022
+#Description: Use this script to install and create the required packages and paths 
+#             for the Client Raspberry Pi
+#----------------------------------------------------------------------------------------------#
+
 from os import system
 from time import sleep
 
 # ------------------------------- raspi-config settings -------------------------------#
+
 system("sudo raspi-config nonint do_change_timezone Europe/Berlin")  # time zone
 system("sudo passwd pi")  # change password
 
@@ -23,12 +31,16 @@ list_pip_install = (
 # list to string
 list_apt_install = " ".join(list_apt_install)
 list_pip_install = " ".join(list_pip_install)
+
 # install all packages
 system("sudo apt-get install %s -y" % list_apt_install)
 system("sudo pip3 install %s" % list_pip_install)
+
 # -------------------------------  download files  -------------------------------#
+
 system("git clone --branch Client https://github.com/Flo0406/Projekt-Pi")
 system("sudo mv /home/pi/Projekt-Pi/Cameraclient.py /home/pi")
+
 # -------------------------------  final reboot -------------------------------#
 
 duration_now = 5
@@ -37,7 +49,7 @@ while duration_now >= 0:
 
     min, sec = divmod(duration_now, 1000)
 
-    print("Everything is finished. Reboot in %d" % sec, end="\r")
+    print("Everything is done. Reboot in %d" % sec, end="\r")
     sleep(1)
     duration_now -= 1
 
